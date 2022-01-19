e200d510566db30d61383c7082277d0752b619a5
mizan <dewan.mizanur911@gmail.com>
2022-01-20 02:40:01 +0600

    products components created for home page

---
+++ src/Pages/Home/Product/Product.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Product.css';
+
+const Product = () => {
+    return (
+        <div>
+            <h3>This is product</h3>
+        </div>
+    );
+};
+
+export default Product;

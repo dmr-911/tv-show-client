948d83bf3f6d64d0502d33c5f62a2f111c48a913
mizan <dewan.mizanur911@gmail.com>
2022-01-20 02:49:52 +0600

    products page updated with fetching data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Products from './Pages/Home/Products/Products';
 
 function App() {
   return (
     <div className="App">
+      <Products></Products>
     </div>
   );
 }
--- src/Pages/Home/Products/Products.js
+++ src/Pages/Home/Products/Products.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Container, Row } from 'react-bootstrap';
+import Product from '../Product/Product';
 import './Products.css';
 
 const Products = () => {
+    const [products, setProducts] = useState([]);
+    useEffect(()=>{
+        fetch('https://api.tvmaze.com/search/shows?q=all')
+        .then(res => res.json())
+        .then(data => setProducts(data))
+    },[]);
+    console.log(products);
+
     return (
-        <div>
-            
-        </div>
+        <Container>
+            <Row xs={1} md={2} lg={3}>
+                {
+                    products.map(product => <Product
+                    key={product.show.id}
+                    product={product}
+                    ></Product>)
+                }
+            </Row>
+        </Container>
     );
 };
 

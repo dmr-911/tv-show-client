b76c54083509d4285852cc3cec73a3586b09a6ad
mizan <dewan.mizanur911@gmail.com>
2022-01-20 05:37:10 +0600

    navigation added

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import NotFound from './Pages/NotFound/NotFound';
 import Details from './Pages/Details/Details';
 import AuthProvider from './context/AuthProvider';
 import Footer from './Pages/Shared/Footer/Footer';
+import Navigation from './Pages/Shared/Navigation/Navigation';
 
 function App() {
   return (
     <div className="App">
       <AuthProvider>
       <Router>
+        <Navigation></Navigation>
         <Routes>
           <Route path="/" element={<Home></Home>}></Route>
           <Route path="home" element={<Home></Home>}></Route>
--- src/Pages/Shared/Navigation/Navigation.js
+++ src/Pages/Shared/Navigation/Navigation.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import './Navigation.css';
+import logo from '../../../images/nav/logo.png';
 
 const Navigation = () => {
     return (
-        <div>
-            <h3>This is navigation</h3>
-        </div>
+        <Navbar className="navigation-bg" variant="dark" expand="lg">
+        <Container fluid>
+            <Navbar.Brand href="#">
+                {/* <img src={logo} alt="" /> */}
+                <img
+        src={logo}
+        width="150"
+        height="60"
+        className="d-inline-block align-top"
+        alt="React Bootstrap logo"
+      />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collapse id="navbarScroll">
+            <Nav
+                className="me-auto my-2 my-lg-0"
+                style={{ maxHeight: '100px' }}
+                navbarScroll
+            >
+                <Nav.Link href="#action1">Home</Nav.Link>
+                <Nav.Link href="#action2">Link</Nav.Link>
+            </Nav>
+            </Navbar.Collapse>
+        </Container>
+        </Navbar>
     );
 };
 
